61fc093b0c09e08910edaff7b5ba44e315a66719
Jonathan Puckey <me@jonathanpuckey.com>
2013-05-05 14:28:07 +0200

    Remove workaround for paperscript tags.

--- helpers/markus/index.js
+++ helpers/markus/index.js
@@ -1,48 +1,20 @@
-var markus = require('./lib/markus'),
-	Html = require('./lib/Html.js'),
-	cheerio = require('cheerio');
+var path = require('path'),
+	fs = require('fs');
 
+var markus = require('./lib/markus');
 
-var tags = ['node', 'block', 'image', 'note', 'code', 'url', 'title', 'column', 'template', 'paperscript', 'nop'];
-
-tags.forEach(function(tag) {
-	markus.register(require('./tags/' + tag));
-});
+// Register all tags contained in tags folder:
+fs.readdirSync(path.join(__dirname, 'tags'))
+	.filter(function(file) {
+		return !(/^\./).test(file);
+	})
+	.forEach(function(file) {
+		markus.register(require('./tags/' + file));
+	});
 
 module.exports = {
 	markus: function(content, param) {
-		if (!content)
-			return '';
-
-		// Because Markus is getting stuck on parsing the script tags,
-		// use cheerio to take out the contents of the scripts and put
-		// them back in after parsing is finished.
-		content = content.replace(/<paperscript/g, '<script type="paperscript"');
-		content = content.replace(/\/paperscript>/g, '/script>');
-		var $ = cheerio.load(content);
-
-		// Find all the script tags and save the source code
-		// in an array, replacing it with 'temp'
-		var scripts = $('script');
-		var sources = [];
-		scripts.each(function(index, script) {
-			var cScript = cheerio(script);
-			sources.push(cScript.html());
-		});
-		scripts.html('temp');
-
-		// Render the content with markus
-		content = markus.parse($.html()).render(param);
-
-		// Put the script sources back again:
-		$ = cheerio.load(content);
-		var count = 0;
-		scripts = $('script');
-		for (var i = 0, l = scripts.length; i < l; i++) {
-			var script = $(scripts[i]);
-			var scriptHtml = script.toString().replace('temp', sources[count++]);
-			script.replaceWith(scriptHtml);
-		}
-		return $.html();
+		if (!content) return '';
+		return markus.parse(content).render(param);
 	}
 };
--- helpers/markus/tags/paperscript.js
+++ helpers/markus/tags/paperscript.js
@@ -1,91 +1,87 @@
 module.exports = {
-	script: function(content, param, encoder, before, after) {
-		if (!this.attributes.type || this.attributes.type != 'paperscript') {
-			var html = '<' + this.definition + (content 
-					? '>' + content + '</' + this.name + '>'
-					: '/>');
-			console.log(html);
-			return html;
-		}
-		var code = content;
-		if (code) {
-			var attrs = this.attributes;
-			var id = attrs.id;
-			if (!id) {
-				param.canvasId = param.canvasId || 0;
-				id = attrs.id = 'canvas-' + (++param.canvasId);
-			}
-			var source = attrs.source;
-			var split = attrs.split === 'true';
-			var explain = attrs.explain === 'true';
-			var inspector = attrs.inspector === 'true';
-			var background = attrs.background;
-			var border = attrs.border;
-			delete attrs.source;
-			delete attrs.split;
-			delete attrs.explain;
-			delete attrs.inspector;
-			delete attrs.background;
-			delete attrs.border;
-			if (!attrs.resize) {
-				if (!attrs.width)
-					attrs.width = 540;
-				if (!attrs.height)
-					attrs.height = 540;
-				if (border) {
-					// Oh CSS why are you such a pain.
-					attrs.width -= 2;
-					attrs.height -= 2;
+	paperscript: {
+		nesting: false,
+		render: function(content, param, encoder, before, after) {
+			var code = content;
+			if (code) {
+				var attrs = this.attributes;
+				var id = attrs.id;
+				if (!id) {
+					param.canvasId = param.canvasId || 0;
+					id = attrs.id = 'canvas-' + (++param.canvasId);
 				}
-			}
-			var inspectorButton = inspector ? '<div class="tools"><div class="button inspector"></div></div>' : '';
-			var parts = [];
-			parts.push(
-				'<div class="paperscript'
-					+ (split ? ' split' : '')
-					+ (source === 'true' ? ' source' : '')
-					+ '">'
-			);
-			if (source !== 'false') {
+				var source = attrs.source;
+				var split = attrs.split === 'true';
+				var explain = attrs.explain === 'true';
+				var inspector = attrs.inspector === 'true';
+				var background = attrs.background;
+				var border = attrs.border;
+				delete attrs.source;
+				delete attrs.split;
+				delete attrs.explain;
+				delete attrs.inspector;
+				delete attrs.background;
+				delete attrs.border;
+				if (!attrs.resize) {
+					if (!attrs.width)
+						attrs.width = 540;
+					if (!attrs.height)
+						attrs.height = 540;
+					if (border) {
+						// Oh CSS why are you such a pain.
+						attrs.width -= 2;
+						attrs.height -= 2;
+					}
+				}
+				var inspectorButton = inspector ? '<div class="tools"><div class="button inspector"></div></div>' : '';
+				var parts = [];
 				parts.push(
-					'<div class="buttons-context">',
-						'<div class="buttons">',
-							inspector && !split ? inspectorButton : '',
-							'<div class="button run">Source</div>',
-							explain ? '<div class="explain"></div>' : '',
-						'</div>'
+					'<div class="paperscript'
+						+ (split ? ' split' : '')
+						+ (source === 'true' ? ' source' : '')
+						+ '">'
 				);
-			}
-			parts.push(
-				'<script type="text/paperscript" canvas="' + id + '">'
-					+ code
-				+ '</script>'
-			);
-			parts.push(
-				'<div class="canvas'
-					+ (inspector && split ? ' buttons-context' : '')
-					+ (border ? ' border' : '')
-					+ '">'
-			);
-
-			if (inspector && split) {
+				if (source !== 'false') {
+					parts.push(
+						'<div class="buttons-context">',
+							'<div class="buttons">',
+								inspector && !split ? inspectorButton : '',
+								'<div class="button run">Source</div>',
+								explain ? '<div class="explain"></div>' : '',
+							'</div>'
+					);
+				}
 				parts.push(
-					'<div class="buttons">',
-						inspectorButton,
+					'<script type="text/paperscript" canvas="' + id + '">'
+						+ code
+					+ '</script>'
+				);
+				parts.push(
+					'<div class="canvas'
+						+ (inspector && split ? ' buttons-context' : '')
+						+ (border ? ' border' : '')
+						+ '">'
+				);
+
+				if (inspector && split) {
+					parts.push(
+						'<div class="buttons">',
+							inspectorButton,
+						'</div>'
+					);
+				}
+				parts.push(
+						'<canvas ' + this.renderAttributes()
+							+ (background ? ' style="background:' + background + '"' : '')
+							+ '></canvas>',
 					'</div>'
 				);
-			}
-			parts.push(
-					'<canvas ' + this.renderAttributes()
-						+ (background ? ' style="background:' + background + '"' : '')
-						+ '></canvas>',
-				'</div>'
-			);
-			if (source !== 'false') {
+				if (source !== 'false') {
+					parts.push('</div>');
+				}
 				parts.push('</div>');
+				return parts.join('\n   ');
 			}
-			parts.push('</div>');
-			return parts.join('\n   ');
 		}
 	}
 };
