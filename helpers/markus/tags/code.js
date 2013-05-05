fc45c44729a1eb4ef3997e678e32954bbd4598c0
Jonathan Puckey <me@jonathanpuckey.com>
2013-05-05 15:46:52 +0200

    Fix code tag.

--- helpers/markus/tags/code.js
+++ helpers/markus/tags/code.js
@@ -1,9 +1,9 @@
 module.exports = {
-	code: function(content, param, encoder, before, after) {
-		var code = content;
-		if (code) {
-			// TODO: Why was the code decoded? Seems to be wrong to do so
-			// code = decode(code);
+	code: {
+		nesting: false,
+		render: function(content, param, encoder, before, after) {
+			var code = content || this.values[0];
+			if (!code) return;
 			// Nesting things in <code> seems to cause conflicts with Helma's
 			// encodeParagraphs that is somehow too smart about it, so use <pre>
 			// for blocks of code and <tt> for inlined code.
@@ -11,16 +11,16 @@ module.exports = {
 					// Handle nested tags differently, e.g. <b><code /></b>
 					// will not have before / after but should not be rendered
 					// as <pre>:
-					|| this.definition == 'root')
+					|| this.parent.definition == 'root')
 					&& (typeof before != 'string' || /[\n\r]$|^$/.test(before))
 					&& (typeof after != 'string' || /^[\n\r]|^$/.test(after))) {
 				// Pass on all attributes to the pre tag.
 				var attributes = this.renderAttributes();
 				return '<pre class="code"'
 						+ (attributes ? ' ' + attributes + '>' : '>')
-						+ code + '</pre>';
+						+ encoder(code) + '</pre>';
 			} else {
-				return '<tt>' + code + '</tt>';
+				return '<tt>' + encoder(code) + '</tt>';
 			}
 		}
 	}
