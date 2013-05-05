8e2d10662d75728f84c83f9cdb512698f8ed6f24
Jonathan Puckey <me@jonathanpuckey.com>
2013-05-05 15:46:15 +0200

    Improve Note tag and add back paragraphs dependency.

--- helpers/markus/tags/note.js
+++ helpers/markus/tags/note.js
@@ -1,16 +1,20 @@
-var Html = require('../lib/Html')
-var defaultTitles = {
-	note: 'Please note:',
-	warning: 'Warning:',
-	tip: 'Did you know?'
-};
-var tags = {};
-var parser = function(content, param, encoder) {
-	var title = this.attributes.title || defaultTitles[this.name];
-	return '<div class="note"><b>' + encoder(title) + '</b>' + Html.formatParagraphs(content) + '<div class="text-end"></div></div>';
-};
-for (var key in defaultTitles) {
-	tags[key] = parser;
-}
+var formatParagraphs = require('../lib/paragraphs')
 
-module.exports = tags;
+module.exports = (new function() {
+	var defaultTitles = {
+		note: 'Please note:',
+		warning: 'Warning:',
+		tip: 'Did you know?'
+	};
+
+	return {
+		'note,warning,tip': {
+			attribute: 'title',
+
+			render: function(content, param, encoder) {
+				var title = this.attributes.title || defaultTitles[this.name];
+				return '<div class="note"><b>' + encoder(title) + '</b>' + formatParagraphs(content) + '<div class="text-end"></div></div>';
+			}
+		}
+	};
+});
