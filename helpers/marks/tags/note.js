59292eee6e8dfc4c6baf4cea2ad604e3d733b197
Jürg Lehni <juerg@scratchdisk.com>
2013-06-21 08:42:04 -0700

    No need for private scope.

--- helpers/marks/tags/note.js
+++ helpers/marks/tags/note.js
@@ -1,20 +1,20 @@
-var formatParagraphs = require('paragraphs')
+var formatParagraphs = require('paragraphs');
 
-module.exports = (new function() {
-	var defaultTitles = {
-		note: 'Please note:',
-		warning: 'Warning:',
-		tip: 'Did you know?'
-	};
+var defaultTitles = {
+	note: 'Please note:',
+	warning: 'Warning:',
+	tip: 'Did you know?'
+};
 
-	return {
-		'note,warning,tip': {
-			attribute: 'title',
+module.exports = {
+	'note,warning,tip': {
+		attribute: 'title',
 
-			render: function(content, param, encode) {
-				var title = this.attributes.title || defaultTitles[this.name];
-				return '<div class="note"><b>' + encode(title) + '</b>' + formatParagraphs(content) + '<div class="text-end"></div></div>';
-			}
+		render: function(content, param, encode) {
+			var title = this.attributes.title || defaultTitles[this.name];
+			return '<div class="note"><b>' + encode(title) + '</b>'
+					+ formatParagraphs(content)
+					+ '<div class="text-end"></div></div>';
 		}
-	};
-});
+	}
+};
