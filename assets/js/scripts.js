82fe7651ec47e5aff1b758243c86a9fee264492f
Jürg Lehni <juerg@scratchdisk.com>
2014-08-20 17:36:42 +0200

    Fix editor resizing.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -415,8 +415,8 @@ function createPaperScript(element) {
 			// Can't get correct dimensions from hidden canvas,
 			// so calculate again.
 			var offset = source.offset();
-			width = $(document).width() - offset.left;
-			height = $(document).height() - offset.top;
+			width = $(window).width() - offset.left;
+			height = $(window).height() - offset.top;
 		}
 		// Resize the main element as well, so that the float:right button
 		// is always positioned correctly.
@@ -426,6 +426,8 @@ function createPaperScript(element) {
 		source
 			.width(width - (hasBorders ? 2 : 1))
 			.height(height - (hasBorders ? 2 : 0));
+		if (editor)
+			editor.refresh();
 	}
 
 	function toggleView() {
