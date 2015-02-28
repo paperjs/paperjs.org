ae014946929baf98bb5865a3bf4d4b6d4b7fb6f7
Jürg Lehni <juerg@scratchdisk.com>
2015-02-28 20:20:10 +0100

    Switch from execSync to child_process.execSync

--- helpers/versionDate.js
+++ helpers/versionDate.js
@@ -1,12 +1,12 @@
-var sh = require('execSync');
+var child_process = require('child_process');
 
 module.exports = {
-	versionDate: function(version, file) {
-		if (/^v/.test(version)) {
-			return new Date(sh.exec('cd paper.js\n\
-				git log -1 --format=%ai ' + version).stdout);
-		} else {
-			return file.created;
-		}
-	}
+    versionDate: function(version, file) {
+        if (/^v/.test(version)) {
+            return new Date(child_process.execSync('cd paper.js\n\
+                git log -1 --format=%ai ' + version).toString());
+        } else {
+            return file.created;
+        }
+    }
 };
