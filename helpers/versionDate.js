d335e4ccff831b59529ae9438ff6a882341061a0
Jürg Lehni <juerg@scratchdisk.com>
2017-04-20 16:36:23 +0200

    Update to latest version of woods and define NPM scripts for dev and build.

--- helpers/versionDate.js
+++ helpers/versionDate.js
@@ -3,7 +3,7 @@ var child_process = require('child_process');
 module.exports = {
 	versionDate: function(version, file) {
 		if (/^v/.test(version)) {
-			return new Date(child_process.execSync('cd paper.js\n\
+			return new Date(child_process.execSync('cd ../paper.js\n\
 				git log -1 --format=%ai ' + version).toString());
 		} else {
 			return file.created;
